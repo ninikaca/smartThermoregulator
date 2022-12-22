c399ea1a9491c7fc2356c6c2242f3ed1fcc251a1
Andrea Čukić <andreacukic55@gmail.com>
2022-12-22 19:14:11 +0100

    realizovana klasa device

--- Smart thermoregulator/Smart thermoregulator/Klase/Device.cs
+++ Smart thermoregulator/Smart thermoregulator/Klase/Device.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime;
 
 namespace Smart_thermoregulator.Klase
 {
-    public class Device
+    public class Device : IDevice
     {
+        private int deviceId;
+        private double temperatura;
+
+        public Device()
+        {
+            this.deviceId = new Random().Next(0, 10000);
+            this.temperatura = new Random().Next(-100, 100);
+        }
+
+        public int DeviceId { get => deviceId; set => deviceId = value; }
+        public double Temperatura { get => temperatura; set => temperatura = value; }
+
+
+        //TO DO: Uraditi metodu
+        public void novoMerenje() { }
+
     }
 }
