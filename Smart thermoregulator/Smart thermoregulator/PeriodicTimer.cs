79c984bce751d1a36e1448bd443f055907e77a21
Andrea Čukić <andreacukic55@gmail.com>
2022-12-22 19:31:05 +0100

    ispravljene greske

---
+++ Smart thermoregulator/Smart thermoregulator/PeriodicTimer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Smart_thermoregulator
+{
+    internal class PeriodicTimer
+    {
+        private TimeSpan timeSpan;
+
+        public PeriodicTimer(TimeSpan timeSpan)
+        {
+            this.timeSpan = timeSpan;
+        }
+
+        internal Task<bool> WaitForNextTickAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
