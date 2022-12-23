046e5e67883d6b5580192cd0ccd6b83bb243d11a
Sara Simović <sarasimovic1111@gmail.com>
2022-12-23 04:27:32 +0100

    regulator

--- Smart thermoregulator/Smart thermoregulator/Klase/Regulator.cs
+++ Smart thermoregulator/Smart thermoregulator/Klase/Regulator.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Smart_thermoregulator.Klase
 {
-    public class Regulator
+    public class Regulator : IRegulator
     {
+        int dnevna_temperatura;
+        int nocna_temperatura;
+
+        int br_sati;
+        int prosecna_temperatura;
+
+        int komanda;
+
+        enum REZIM { DNEVNI_REZIM, NOCNI_REZIM };
+
+        public int DNEVNA { get => dnevna_temperatura; set => dnevna_temperatura = value; }
+        public int NOCNA { get => nocna_temperatura; set => nocna_temperatura = value; }
+        public int KOMANDA { get => komanda; set => komanda = value; }
+
+        public void regulacija()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
