2525a03b7d6daa8a24e9aa4f6e36a41ad334ce27
Katarina Marinković <94901949+ninikaca@users.noreply.github.com>
2022-12-23 04:44:06 +0100

    Add files via upload
    Refactor celog projekta :)

---
+++ Smart Thermoregulator  Refactor/Common/Implementacije/Log.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Implementacije
+{
+    public class Log : ILog
+    {
+        private string poruka_za_upis;
+
+        public Log(string poruka)
+        {
+            if (poruka == null)
+                throw new ArgumentNullException();
+
+            if (poruka.Trim().Length == 0)
+                throw new ArgumentException();
+
+            poruka_za_upis = poruka;
+        }
+
+        // metoda koja sluzi za belezenje informacionih poruka
+        public void LogNoveErrorPoruke(string poruka)
+        {
+            throw new NotImplementedException();
+        }
+
+        // metoda koja sluzi za belezenje poruka o upozorenju
+        public void LogNoveInformationPoruke(string poruka)
+        {
+            throw new NotImplementedException();
+        }
+
+        // metoda koja sluzi za belezenje poruka o gresci
+        public void LogNoveWarningPoruke(string poruka)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
