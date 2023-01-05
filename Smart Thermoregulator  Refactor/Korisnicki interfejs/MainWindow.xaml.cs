042684c3fac19d2716444a268967b207223cf914
Iva Samopjan <samopjaniva@yahoo.com>
2023-01-05 18:30:47 +0100

    UI / cuvanje temperature uredjaja

--- Smart Thermoregulator  Refactor/Korisnicki interfejs/MainWindow.xaml.cs
+++ Smart Thermoregulator  Refactor/Korisnicki interfejs/MainWindow.xaml.cs
@@ -32,9 +32,44 @@ namespace Korisnicki_interfejs
             }
         }
 
-        private void IspisLoga_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // zatvaranje programa
+            Close();
+        }
 
+        private void cuvanjeDnevneTempBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (uredjaji.GetDevices().Count < 4)
+            {
+                MessageBox.Show("Potrebno je dodati minimalno 4 uređaja!", "Upozorenje", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (pocetakDnevna.SelectedIndex != -1 && krajDnevna.SelectedIndex != -1 && !tempDnevna.Text.Equals(""))
+            {
+                try
+                {
+                    int pd = Int32.Parse(pocetakDnevna.Text);
+                    int kd = Int32.Parse(krajDnevna.Text);
+
+                    if (pd > kd)
+                    {
+                        MessageBox.Show("Nije moguće sačuvati podešavanja!\nUnelite ste nevalidne vrednosti!", "Upozorenje", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        return;
+                    }
+
+                    CheckFields(pd, kd);
+                    SetupUredjaja();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Niste uneli broj!", "Greška", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nije moguće sačuvati podešavanja!\nUneli ste nevalidne vrednosti!", "Upozorenje", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
 
     }
