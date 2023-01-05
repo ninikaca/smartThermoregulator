717a2cc2e24e6d92e06ce93433dd0110dc933f95
Iva Samopjan <samopjaniva@yahoo.com>
2023-01-05 18:30:23 +0100

    UI / dodaj novi uredjaj

--- Smart Thermoregulator  Refactor/Korisnicki interfejs/MainWindow.xaml.cs
+++ Smart Thermoregulator  Refactor/Korisnicki interfejs/MainWindow.xaml.cs
@@ -1,33 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Common.Implementacije;
+using Heater;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Korisnicki_interfejs
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        DeviceCollection uredjaji = new DeviceCollection();
+        Regulator regulator = null;
         public MainWindow()
         {
             InitializeComponent();
+
+            // povezivanje datagrid i kolekcije uredjaja
+            aktivniUredjaji.ItemsSource = uredjaji.GetDevices();
         }
 
         private void dodajNoviUredjajBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (uredjaji.AddDevice())
+            {
+                MessageBox.Show("Uređaj je uspešno dodat!", "Informacija", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Nije moguće dodati novi uređaj!\nPrekoračili ste limit uređaja za dodavanje!", "Upozorenje", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
 
         private void IspisLoga_Click(object sender, RoutedEventArgs e)
