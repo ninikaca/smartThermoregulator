753c8dcff523abb480661a5ec1ffd849d0e49b42
Katarina Marinkovic <ninikaca@gmail.com>
2023-01-05 18:43:31 +0100

    logger radi (donekle)

--- Smart Thermoregulator  Refactor/Logger/Implementacije/Log.cs
+++ Smart Thermoregulator  Refactor/Logger/Implementacije/Log.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Logger.Interfejsi;
+using System;
 
-namespace Logger.Implementacije
+namespace Logger
 {
-    class Log
+    public class Log : ILog
     {
+        private string poruka_za_upis;
+        LogPoruka logPoruka = null;
+        LogUFajl logUFajl = null;
+
+        public Log(string poruka)
+        {
+            if (poruka == null)
+                throw new ArgumentNullException();
+
+            if (poruka.Trim().Length == 0)
+                throw new ArgumentException();
+
+            poruka_za_upis = poruka;
+
+            logPoruka = new LogPoruka(poruka);
+        }
+
+        // metoda koja sluzi za belezenje informacionih poruka
+        public void LogNoveErrorPoruke()
+        {
+            logUFajl = new LogUFajl("log.txt");
+            logUFajl.UpisULogDatoteku(logPoruka.GetErrorMessage());
+        }
+
+        // metoda koja sluzi za belezenje poruka o upozorenju
+        public void LogNoveInformationPoruke()
+        {
+            logUFajl = new LogUFajl("log.txt");
+            logUFajl.UpisULogDatoteku(logPoruka.GetInfoMessage());
+        }
+
+        // metoda koja sluzi za belezenje poruka o gresci
+        public void LogNoveWarningPoruke()
+        {
+            logUFajl = new LogUFajl("log.txt");
+            logUFajl.UpisULogDatoteku(logPoruka.GetWarnMessage());
+        }
     }
 }
