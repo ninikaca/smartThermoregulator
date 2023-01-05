abc7cebb8cc4b53bdc2d708e94da0dbcc2e0ea71
Andrea Cukic <cukic.pr145.2020@uns.ac.rs>
2023-01-05 18:16:03 +0100

    device + heater

--- Smart Thermoregulator  Refactor/Heater/Implementacije/Heater.cs
+++ Smart Thermoregulator  Refactor/Heater/Implementacije/Heater.cs
@@ -9,5 +9,35 @@ namespace Heater
 {
     public class Heater : IHeater
     {
+        public bool IsHeaterOn { get; set; }
+        public ObservableCollection<Device.Device> devices { get; set; }
+
+        Stopwatch ProtekloVreme { get; set; }
+
+        public Heater()
+        {
+            IsHeaterOn = false;
+            ProtekloVreme = new Stopwatch();
+        }
+        public void PokreniZagrevanje()
+        {
+            if (IsHeaterOn) // ako je izdata komanda za paljenje grejaca
+            {
+                ProtekloVreme.Start(); // pokreni brojanje koliko je grejac radio
+
+                foreach (Device.Device d in devices)
+                {
+                    d.Temperatura += 0.01;
+                }
+            }
+            else
+            {
+                ProtekloVreme.Stop(); // zaustavi rad i upisi u evidenciju
+                Evidencija();
+                ProtekloVreme.Reset(); // ponovno brojanje od nule
+            }
+        }
+
+        public void Evidencija()
     }
 }
